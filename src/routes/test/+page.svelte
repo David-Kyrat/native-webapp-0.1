285b44174ff55afbee35b24cc7a51daae5c9291a
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-08-25 16:00:34 +0200

    added test with href attributes render as anchor elements -- exactly what needed

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -13,7 +13,7 @@
     src="https://en.wikipedia.org/wiki/Home"
     title="Selected Tab"
     name="href-tabs-frame"
-    style="width: 100%; height: 400px; border: 0;"
+    style="width: 100%; height: 50em; border: 0;"
   />
 </div>
 
