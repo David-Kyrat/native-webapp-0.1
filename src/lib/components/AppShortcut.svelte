a693e711e989b54c70e83cf74e43e48e880e8b0b
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-08-25 16:38:35 +0200

    implemented card for app shortcut - link doesnt work tho

---
+++ src/lib/components/AppShortcut.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Card, { Content, PrimaryAction, Media, MediaContent } from '@smui/card';
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	export let title = 'Title';
+	export let icon = 'https://placehold.co/320x320?text=square';
+
+	export let url;
+	$: new_url = url && url.length > 0 ? url : 'https://google.com';
+
+	async function open_window() {
+		await invoke('open_window', { url: url });
+	}
+</script>
+
+<div class="card-container">
+	<Card style="min-width: 100px;">
+		<PrimaryAction on:click={() => open_window()}>
+			<Media class="card-media-16x9" aspectRatio="16x9" style="background-image: url({icon}); ">
+				<div style="color: #fff; position: absolute; bottom: 5px; left: 16px;">
+					{#if icon.includes('placehold.co')}
+						<h3 class="mdc-typography--Caption" style="margin: 0;">{title}</h3>
+					{/if}
+				</div>
+			</Media>
+		</PrimaryAction>
+	</Card>
+</div>
+
+<style>
+	.card-container {
+		width: 17%;
+	}
+
+	* :global(.card-media-16x9) {
+		margin: 0.5em 0em;
+		background-size: contain;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
 	import FancyBtn from '$lib/components/FancyBtn.svelte';
 	import { invoke } from '@tauri-apps/api/tauri';
+	import AppShortcut from '$lib/components/AppShortcut.svelte';
+
+	let clicked = 0;
 
 	let input_url;
 	$: new_url = input_url && input_url.length > 0 ? input_url : 'https://google.com';
@@ -14,20 +17,20 @@
 
 <h1>Mail</h1>
 
-<input
-	id="greet-input"
-	placeholder="Enter website url..."
-	bind:value={input_url}
-/>
+<input id="greet-input" placeholder="Enter website url..." bind:value={input_url} />
 
 <FancyBtn text="Open window" , on_click={open_window} />
 
 <p>Going to {new_url}</p>
 
+<AppShortcut title="Gmail" icon="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" />
+
+
 <style>
 	input {
-        width: 80%;
-        padding: 0.5em;
-        margin: 2em 0.3em;
+		width: 80%;
+		padding: 0.5em;
+		margin: 2em 0.3em;
+		margin-bottom: 0;
 	}
 </style>
