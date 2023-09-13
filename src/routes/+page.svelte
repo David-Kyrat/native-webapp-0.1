ef703976c362058a45916fddb6e04ba3a06b0392
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-09-13 13:31:03 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
 	import AppShortcut from '$lib/components/AppShortcut.svelte';
-	// import Elevation from './_Elevation.svelte';
+	import ImageList, {
+		Item,
+		ImageAspectContainer,
+		Image,
+		Supporting,
+		Label
+	} from '@smui/image-list';
 
 	let app_shortcuts = [
 		{
@@ -53,6 +59,19 @@
 		<AppShortcut title={ap.title} icon={ap.icon} url={ap.url} styleclass="" />
 	{/each}
 </div>
+
+<ImageList class="my-image-list-standard">
+	{#each Array(15) as _unused, i}
+		<Item>
+			<ImageAspectContainer>
+				<Image src="https://placehold.co/190x190?text=square" alt="Image {i + 1}" />
+			</ImageAspectContainer>
+			<Supporting>
+				<Label>Image {i + 1}</Label>
+			</Supporting>
+		</Item>
+	{/each}
+</ImageList>
 
 <hr />
 
