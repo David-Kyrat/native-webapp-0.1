afc03d05045fb1d76e0ef5bf3e623468361fd12b
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2024-03-21 13:55:06 +0100

    fix: fixed conflict due to browser test put in master instead of test branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,37 @@
 <script>
-	import AppShortcut from '$lib/components/AppShortcut.svelte';
-	import ImageList, {
-		Item,
-		ImageAspectContainer,
-		Image,
-		Supporting,
-		Label
-	} from '@smui/image-list';
-	import Paper, { Title, Content } from '@smui/paper';
-	import LayoutGrid, { Cell } from '@smui/layout-grid';
-	import { page } from '$app/stores';
-	$: is_active = $page.url.pathname === '/';
+  import AppShortcut from "$lib/components/AppShortcut.svelte"
+  // import ImageList, { Item, ImageAspectContainer, Image, Supporting, Label } from "@smui/image-list"
+  import { Supporting, Label } from "@smui/image-list"
+  import Paper, { Title, Content } from "@smui/paper"
+  import LayoutGrid, { Cell } from "@smui/layout-grid"
+  /* import { page } from "$app/stores"
+  $: is_active = $page.url.pathname === "/" */
 
-	let app_shortcuts = [
-		{
-			title: 'ProtonPass',
-			icon: 'https://static.wikia.nocookie.net/logopedia/images/e/e4/Proton_Pass_Icon.svg',
-			url: 'https://pass.proton.me'
-		},
-		{
-			title: 'ProtonMail',
-			icon: 'https://www.privacyguides.org/en/assets/img/email/protonmail.svg',
-			url: 'https://mail.proton.me'
-		},
-		{
-			title: 'UniMail',
-			icon: 'https://upload.wikimedia.org/wikipedia/commons/d/df/Microsoft_Office_Outlook_%282018%E2%80%93present%29.svg',
-			url: 'http://outlook.com/owa/unige.ch/munz3@unigech.mail.onmicrosoft.com'
-		},
-		{
-			title: 'Gmail',
-			icon: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg',
-			url: 'https://mail.google.com/mail/u/0/#inbox'
-		}
-	];
-	// $: app_nb = app_shortcuts.length;
-	// let col_nb = 4;
-	/* Still doesnt work consistently :
+  let app_shortcuts = [
+    {
+      title: "ProtonPass",
+      icon: "https://static.wikia.nocookie.net/logopedia/images/e/e4/Proton_Pass_Icon.svg",
+      url: "https://pass.proton.me"
+    },
+    {
+      title: "ProtonMail",
+      icon: "https://www.privacyguides.org/en/assets/img/email/protonmail.svg",
+      url: "https://mail.proton.me"
+    },
+    {
+      title: "UniMail",
+      icon: "https://upload.wikimedia.org/wikipedia/commons/d/df/Microsoft_Office_Outlook_%282018%E2%80%93present%29.svg",
+      url: "http://outlook.com/owa/unige.ch/munz3@unigech.mail.onmicrosoft.com"
+    },
+    {
+      title: "Gmail",
+      icon: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg",
+      url: "https://mail.google.com/mail/u/0/#inbox"
+    }
+  ]
+  // $: app_nb = app_shortcuts.length;
+  // let col_nb = 4;
+  /* Still doesnt work consistently :
     let grid_cols = "grid-cols-3";
 	$: grid_cols = `grid-cols-${col_nb}`;
 	let gap = `gap-8`;
@@ -54,40 +49,25 @@
 <!-- {/each} -->
 <!-- </div> -->
 
-<!-- <div class="bg-gray-200"> -->
 <Paper class="max-w-[95%] mx-auto">
-	<Title>Apps</Title>
-	<Content>
-		<!-- <ImageList class="my-image-list-standard"> -->
-		<LayoutGrid>
-			{#each app_shortcuts as ap}
-				<Cell>
-					<!-- <Item class="my-0"> -->
-					<!-- <ImageAspectContainer> -->
-					<AppShortcut title={ap.title} icon={ap.icon} url={ap.url} styleclass="" />
-					<Supporting class="!justify-center">
-						<Label class="text-center">{ap.title}</Label>
-					</Supporting>
-					<!-- </ImageAspectContainer> -->
-					<!-- </Item> -->
-				</Cell>
-			{/each}
-			<!-- </ImageList> -->
-		</LayoutGrid>
-	</Content>
+  <Title>Apps</Title>
+  <Content>
+    <LayoutGrid>
+      {#each app_shortcuts as ap}
+        <Cell>
+          <AppShortcut title={ap.title} icon={ap.icon} url={ap.url} styleclass="" />
+          <Supporting class="!justify-center">
+            <Label class="text-center">{ap.title}</Label>
+          </Supporting>
+        </Cell>
+      {/each}
+    </LayoutGrid>
+  </Content>
 </Paper>
 
-<!-- </div> -->
-
 <style>
-	/*input {
-		width: 80%;
-		padding: 0.5em;
-		margin: 2em 0.3em;
-		margin-bottom: 0;
-	}*/
-	* :global(AppShortcut) {
-		display: inline-block;
-		padding: 20em;
-	}
+  * :global(AppShortcut) {
+    display: inline-block;
+    padding: 20em;
+  }
 </style>
